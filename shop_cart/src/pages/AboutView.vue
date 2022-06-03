2e3b92fcb277ccd7ee680dc30fe905f5abb1279d
nayHw <scm.nayhlaingwin@gmail.com>
2022-06-03 09:11:07 +0630

    updated

--- shop_cart/src/pages/AboutView.vue
+++ shop_cart/src/pages/AboutView.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1 class="text-primary">This is an about page</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col">  
+        <div class="card pb-1">
+          <div class="card-body p-2">
+            <div class="row">
+              <div class="col-md-4">
+                <img alt="Vue logo" src="../assets/images/logo_2.png" class="w-100">
+              </div>
+              <div class="col-md-8 pt-2">
+                <h4 class="card-title text-info text-uppercase">Special title treatment</h4>
+                <p class="card-text">
+                  With supporting text below as a natural lead-in to additional content.
+                  With supporting text below as a natural lead-in to additional content.
+                  With supporting text below as a natural lead-in to additional content.
+                  With supporting text below as a natural lead-in to additional content.
+                  With supporting text below as a natural lead-in to additional content.
+                  With supporting text below as a natural lead-in to additional content.
+                  With supporting text below as a natural lead-in to additional content.
+                  With supporting text below as a natural lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- shop_cart/src/pages/ProductView.vue
+++ shop_cart/src/pages/ProductView.vue
@@ -4,7 +4,7 @@
       <div class="col-md-3">
         <div class="card pb-1">
           <div class="card-body p-2">
-            <img alt="Vue logo" src="../assets/logo.png" class="w-100">
+            <img alt="Vue logo" src="../assets/images/logo.png" class="w-100">
             <h5 class="card-title" @click="show = true">Special title treatment</h5>
             <div class="pb-3">
               <span class="btn outline-none py-1 pl-0">$50</span>
@@ -23,10 +23,10 @@
           <div class="card-body p-2">
             <div class="row">
               <div class="col-md-4">
-                <img alt="Vue logo" src="../assets/logo.png" class="w-100">
+                <img alt="Vue logo" src="../assets/images/logo.png" class="w-100">
               </div>
               <div class="col-md-8 pt-5">
-                <h4 class="card-title">Special title treatment</h4>
+                <h4 class="card-title pt-4">Special title treatment</h4>
                 <div class="pb-3 w-50">
                   <span class="btn outline-none py-1 pl-0">Price: $50</span>
                   <span class="float-right btn btn-info py-1 px-4">vuex</span>
